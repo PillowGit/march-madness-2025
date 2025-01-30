88e3d2a8ef5841baa2055611535535f8f464f78b
PillowGit <goldentaco727@gmail.com>
2025-01-30 14:35:25 -0800

    Nuxt.js fully setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  // Link css to all pages from main.css
+  css: ["@/assets/css/main.css"],
+  // Add tailwindcss module
+  modules: ["@nuxtjs/tailwindcss"],
+  // Add google fonts
+  app: {
+    head: {
+      link: [
+        { rel: "preconnect", href: "https://fonts.googleapis.com" },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossorigin: "anonymous",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Nunito:ital@0;1&display=swap",
+        },
+      ],
+    },
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script>
+import Hello from "@/components/Hello.vue";
+</script>
+
+<template>
+  <div
+    class="min-w-screen min-h-screen flex flex-col items-center justify-center"
+  >
+    <Hello />
+  </div>
+</template>
